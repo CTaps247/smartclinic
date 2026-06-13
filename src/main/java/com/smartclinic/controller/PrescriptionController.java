8038ebf14daca772f87ae87442d21e17f103d3eb
Christian Tapia <ctaps1024@gmail.com>
2026-06-13 08:49:07 -0700

    Delete Prescription entity from controller
    Removed Prescription entity class from PrescriptionController.

--- src/main/java/com/smartclinic/controller/PrescriptionController.java
+++ src/main/java/com/smartclinic/controller/PrescriptionController.java
@@ -35,15 +35,3 @@ public class PrescriptionController {
     }
 }
 
-@Entity
-public class Prescription {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String medication;
-    private String dosage;
-
-    // getters and setters
-}
