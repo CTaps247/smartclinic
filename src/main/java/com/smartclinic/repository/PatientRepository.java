9c7c6a124dabcbc8532491ad42f40479ff9c96cd
CTaps247 <ctaps1024@gmail.com>
2026-06-13 02:42:31 -0400

    Add PatientRepository

--- src/main/java/com/smartclinic/repository/PatientRepository.java
+++ src/main/java/com/smartclinic/repository/PatientRepository.java
@@ -2,6 +2,8 @@ package com.smartclinic.repository;
 
 import com.smartclinic.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 }
